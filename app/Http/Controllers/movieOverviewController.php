f0ab60a84177dcc71e3850530f1bd53748dbc3f1
Tom Vrijmoet <tfjvrijmoet@hotmail.com>
2016-09-29 01:01:34 +0200

    Working on MVC
    Trying to get something out of the MVC idea, not working out too well.

--- app/Http/Controllers/movieOverviewController.php
+++ app/Http/Controllers/movieOverviewController.php
@@ -6,7 +6,15 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\movieOverview;
+
 class movieOverviewController extends Controller
 {
-    //
+    public function index(){
+
+        //Show all the data from the "movies" table
+        $movies = movieOverview::all();
+        
+        return view('moviesOverview',['movies' => $movies]);
+    }
 }
--- app/movieOverview.php
+++ app/movieOverview.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class movieOverview extends Model
 {
-    //
+    protected $table = 'movies';
+    
+    public $timestamps = false;
 }
